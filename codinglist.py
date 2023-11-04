b80108d91820da062b044a93fac2c43c1667bc6b
NimiAndKevin <146501295+NimiAndKevin@users.noreply.github.com>
2023-11-04 15:22:10 -0700

    Add files via upload

---
+++ codinglist.py
@@ -0,0 +1,7 @@
+Friends=["Matthew", "Adam", "SJ", "Kevin"]
+print(f"{Friends[0]} is a boy")
+print(Friends[1]+" is a boy")
+print(Friends[2]+" is a boy")
+print(Friends[3]+" is a boy")
+print(Friends[2]+" is a boy and his partner is laughing " + Friends[3])
+print(Friends[1]+" is a boy and his boogyman is dancing "+ Friends[2]+" while his bestie is playing with his phone "+ Friends[3])print(Friends[3]+" is a boy and his partner is using the phone "+Friends[0]+" while his middle man is playing with "+ Friends[2] +" then his side kick went to the ocean " + Friends[1])
